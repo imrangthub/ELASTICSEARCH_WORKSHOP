256c5a2315b84ee4814de93fb644827b94ae7456
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2021-10-12 19:24:12 +0600

    add crud

---
+++ spring-boot-elasticsearch-crud/src/main/java/com/imranmadbar/customer/CustomerEntity.java
@@ -0,0 +1,54 @@
+package com.imranmadbar.customer;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.io.Serializable;
+
+@Document(indexName="imranmadbar",type="customer",shards=1)
+@ToString
+public class CustomerEntity  implements Serializable {
+
+    private static final long serialVersionUID = -4538971070304943261L;
+
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private int age;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ spring-boot-elasticsearch-crud/src/main/java/com/imranmadbar/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.imranmadbar.customer;
+
+import java.util.List;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+
+public interface CustomerRepository extends ElasticsearchRepository<CustomerEntity, String>{
+
+    List<CustomerEntity> findByFirstname(String firstName);
+
+}
